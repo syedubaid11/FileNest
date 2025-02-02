c66efa422ab23171a8019e173c7110e7634b7b7a
installer <syedabuubaid@gmail.com>
2025-02-02 17:42:53 +0530

    commit

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,10 +1,16 @@
-import {integer, varchar , pgTable} from 'drizzle-orm/pg-core'
+import {integer, varchar , pgTable , serial , timestamp , boolean , uuid} from 'drizzle-orm/pg-core'
 
 
 export const usersTable=pgTable("users",{
     id:integer().primaryKey(),
     name:varchar({length:255}).notNull(),
-    age:integer().notNull()
-
+    email:varchar({length:255}).notNull(),
 
 })
+
+export const FileTable=pgTable("files",{
+    id:serial("id").primaryKey(),
+    uploadedAt:timestamp().notNull(),
+    isPublic:boolean().notNull(),
+})
+
