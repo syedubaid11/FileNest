e39aaae045380aeeaeaae5b1d7929f7e9421dec7
installer <syedabuubaid@gmail.com>
2025-02-04 02:12:41 +0530

    commit

--- backend/src/files/files.service.ts
+++ backend/src/files/files.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
+
 @Injectable()
 export class FilesUploadService {
     handleFileUpload(file:Express.Multer.File): string {
-        return `File uploaded successfully, ${file.originalname}`;
+        return `File uploaded successfully, ${file.path}`;
     }
 }
