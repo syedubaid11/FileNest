618743c422f4f545f376c275e880b99f7a411753
installer <syedabuubaid@gmail.com>
2025-02-07 19:13:47 +0530

    commit

--- backend/src/files/files.service.ts
+++ backend/src/files/files.service.ts
@@ -1,27 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
-
+require ('dotenv').config();
 
 @Injectable()
 export class FilesUploadService {
-    // private supabase = createClient(
-    //     process.env.SUPABASE_URL as string,
-    //     process.env.SUPABASE_KEY as string
-    // ); 
-    // handlesupabase(){
-    //     if(this.supabase){
-    //         return 'supabase is connected'
+    private supabase = createClient(
+        process.env.SUPABASE_URL as string,
+        process.env.SUPABASE_KEY as string
+    ); 
+    handlesupabase(){
+        if(this.supabase){
+            return 'supabase is connected'
 
-    //     }
-    //     else {
-    //         return 'supabase not connected'
-    //     }
-    // }
+        }
+        else {
+            return 'supabase not connected'
+        }
+    }
     
 
     handleFileUpload(file:Express.Multer.File): string {
-        return `File uploaded successfully, ${file.path},${process.env.SUPABASE_URL}`;
+        return `File uploaded successfully,`;
         //do error handling if file is not uploaded
     }
 }
