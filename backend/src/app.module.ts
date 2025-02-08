0a8697fa128a9fa8d4d27c26409376a85b4c8d39
installer <syedabuubaid@gmail.com>
2025-02-08 21:48:25 +0530

    commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilesModule } from './files/files.module';
 import { UsersModule } from './users/users.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [FilesModule, UsersModule],
+  imports: [FilesModule, UsersModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,6 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService:UsersService){}
+
+    @Get(':id')
+    async getUser(@Param('id') id:string){
+        return this.usersService.findByid(id);
+    }
+
+
 }
