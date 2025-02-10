262bb55b0c843795f6ef7f8893e9f46d9d9a625a
installer <syedabuubaid@gmail.com>
2025-02-11 00:07:32 +0530

    commit

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 
 @Controller('admin')
-export class AdminController {}
+@UseGuards(AdminGuard)
+export class AdminController {
+    @Get('dashboard')
+    getDashboard(){
+        return {message:'welcome to the admin dashboard'}
+
+    }
+}
---
+++ backend/src/admin/admin.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    return true;
+  }
+}
+
+//canActivate(context:executionconte)) 
