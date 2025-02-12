116923a39d852288dcdced025ac9251ac885aaf8
installer <syedabuubaid@gmail.com>
2025-02-13 01:50:35 +0530

    commit

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono , Bebas_Neue} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+});
+
+const bebasNeue= Bebas_Neue({
+  weight:"400"
 });
 
 const geistMono = Geist_Mono({
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,11 +3,17 @@ import FadingDotsBackground from "./components/fading-dots-background";
 export default function Home(){
   return(
     <>
-    <FadingDotsBackground/>
-    <div className="z-10 text-white text-center">
+    <div className="z-1">
+      <FadingDotsBackground/>
+    </div>
+    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="z-20 text-white flex flex-col items-center">
         <h1 className="text-4xl font-bold mb-4">Welcome to FileNest</h1>
         <p className="text-xl">A blazing fast way to share files</p>
       </div>
+
+    </div>
+    
     </>
   )
 }
