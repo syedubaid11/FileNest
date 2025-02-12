8c215f8036ddff89a104ef6fd73f58e0957cc5cc
installer <syedabuubaid@gmail.com>
2025-02-13 02:34:48 +0530

    commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,14 @@
 import FadingDotsBackground from "./components/fading-dots-background";
+import { Bebas_Neue } from "next/font/google";
+
+const bebasNeue= Bebas_Neue({
+  variable:'--font-bebas-neue',
+  weight:"400"
+});
 
 export default function Home(){
+ 
+   
   return(
     <>
     <div className="z-1">
@@ -8,8 +16,8 @@ export default function Home(){
     </div>
     <div className="w-screen h-screen flex items-center justify-center">
     <div className="z-20 text-white flex flex-col items-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to FileNest</h1>
-        <p className="text-xl">A blazing fast way to share files</p>
+        <h1 className={`${bebasNeue.className} text-8xl font-bold mb-4`}>Welcome to FileNest</h1>
+        <p className={`${bebasNeue.className} text-4xl`}>A blazing fast way to share files</p>
       </div>
 
     </div>
