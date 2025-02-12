949210c4003f35b954dcaa5b6603c17bd343e324
installer <syedabuubaid@gmail.com>
2025-02-13 02:08:36 +0530

    commit

---
+++ frontend/app/pages/dashboard.tsx
@@ -0,0 +1,15 @@
+//returns react node/children components 
+interface DashboardProps{
+    children:React.ReactNode,
+}
+
+
+
+export const Dashboard:React.FC<DashboardProps>=()=>{
+    return(
+        <>
+
+        </>
+    )
+
+}
