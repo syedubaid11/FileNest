a219986c8430918faaf5a4154319863687325ad2
installer <syedabuubaid@gmail.com>
2025-02-21 02:27:13 +0530

    commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home(){
     </div>
     <div className="w-screen h-screen flex items-center justify-center">
     <div className="z-20 text-white flex flex-col items-center">
-        <h1 className={`${bebasNeue.className} text-8xl font-bold mb-4 animate`}>Welcome to FileNest</h1>
+        <h1 className={`${bebasNeue.className} text-8xl font-bold mb-4 `}><span className="animate">Welcome to</span> <span className="animate-2">FileNest</span></h1>
         <p className={`${bebasNeue.className} text-4xl animate-2`}>A blazing fast way to share files</p>
       </div>
 
