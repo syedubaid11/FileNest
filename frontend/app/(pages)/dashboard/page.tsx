4f9be376512bb413c386fb4e2bb9480ad4bfa976
installer <syedabuubaid@gmail.com>
2025-03-03 14:36:33 +0530

    commit

--- frontend/app/(pages)/dashboard/page.tsx
+++ frontend/app/(pages)/dashboard/page.tsx
@@ -41,26 +41,36 @@ export default function Dashboard(){
                 </div>
                 
                 {/* upload - bottom section */}
-                <div className="flex flex-col items-start gap-4 mt-4">
-                    <div className="w-full max-w-2xl rounded-lg border-dashed h-[300px] border-neutral-500 border">
-                        <div className="h-full w-full flex items-center justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-12">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
-                            </svg>
+                <div className="flex flex-row items-start gap-4 mt-4">
+                    <div className="flex flex-col w-full">
+                        <div className="w-full max-w-2xl rounded-lg border-dashed h-[300px] border-neutral-500 border">
+                                <div className="h-full w-full flex items-center justify-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-12">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                                    </svg>
+                                </div>
+                        </div>
+                        <div className="flex w-full max-w-2xl justify-center">
+                                <Button variant="outline" size="lg" onClick={async () => { 
+                                    const result = await axios.post('http://localhost:3001/files/upload');
+                                    if(result.status === 200){
+                                        console.log("file uploaded")
+                                    }
+                                    else{
+                                        console.log('error while fetching api')
+                                    }
+                                }}>Upload</Button>
                         </div>
                     </div>
-                    <div className="flex w-full max-w-2xl justify-center">
-                        <Button variant="outline" size="lg" onClick={async () => { 
-                            const result = await axios.post('http://localhost:3001/files/upload');
-                            if(result.status === 200){
-                                console.log("file uploaded")
-                            }
-                            else{
-                                console.log('error while fetching api')
-                            }
-                        }}>Upload</Button>
+
+                    <div className="w-[400px] h-[300px] border border border-neutral-500 mr-[50px] rounded-lg">
+
                     </div>
+
                 </div>
+
+
+
             </div>
         </div>
     )
