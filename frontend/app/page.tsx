6e9c61b0fef8073bfa07a12e04431e67c5598d9c
installer <syedabuubaid@gmail.com>
2025-03-09 00:35:05 +0530

    commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home(){
     <div className="w-screen h-screen flex items-center justify-center">
     <div className="z-20 text-white flex flex-col items-center">
         <h1 className={`${bebasNeue.className} text-8xl font-bold mb-4 `}><span className="animate">Welcome to</span> <span className="animate-2">FileNest</span></h1>
-        <p onClick={()=>{router.push('/dashboard')}}className={`${bebasNeue.className} text-[40px] animate-2 flex flex-row items-center hover:cursor-pointer transition-all duration-300 hover:text-[50px] hover:text-gray-500`}>A blazing fast way to share files<span className="ml-[6px]"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <p onClick={()=>{router.push('/dashboard')}}className={`${bebasNeue.className} text-[40px] animate-2 flex flex-row items-center hover:cursor-pointer transition-all duration-300 hover:text-[50px] hover:text-gray-500`}>A blazing fast way to share files<span className="ml-[6px]"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
 </svg>
 </span></p>
--- frontend/components/ui/navbar.tsx
+++ frontend/components/ui/navbar.tsx
@@ -1,8 +1,12 @@
 import { RedirectToSignIn } from "@clerk/nextjs"
+import Image from "next/image"
+import logo from '../../assets/filenest.png'
+
+
 export const Navbar=()=>{
     return(
         <div className="flex justify-between w-screen items-center p-4 text-white">
-            <h1 className="text-2xl">logo</h1>
+            <div className="h-[150px] w-[150px]"><Image src={logo} alt="logo"></Image></div>
             <div className="flex items-center">
             </div>
         </div>  
