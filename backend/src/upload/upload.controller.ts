b517e17b237c32775e32cfba0fe01d7b7c7f0353
syedubaid <syedabuubaid@gmail.com>
2025-03-24 16:19:17 +0530

    feat:added presigned url

--- backend/src/upload/upload.controller.ts
+++ backend/src/upload/upload.controller.ts
@@ -1,5 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { UploadService } from './upload.service';
 
 @Controller('upload')
 export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Get('presigned-url')
+  async getPresignedUrl(@Query('filename') filename: string, @Query('fileType') fileType: string) {
+    return {
+      presignedUrl: await this.uploadService.getPresignedUrl(filename, fileType),
+    };
+  }
 }
--- backend/src/upload/upload.service.ts
+++ backend/src/upload/upload.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import {S3Client,} from "@aws-sdk/client-s3";
+import {S3Client, PutObjectCommand} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 
 dotenv.config();
-
 @Injectable()
 export class UploadService {
     private s3:S3Client;
@@ -13,12 +13,26 @@ export class UploadService {
     constructor(){ 
         this.s3=new S3Client({
             region:'auto',
-            endpoint:process.env.R2_ENDPOINT
+            endpoint:process.env.R2_ENDPOINT,
+            credentials: {
+                accessKeyId: process.env.R2_ACCESS_ID!,
+                secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+            },
 
         })
-    }
+    } 
+    
+    
+  async getPresignedUrl(filename: string, fileType: string): Promise<string> {
+    const command = new PutObjectCommand({
+      Bucket: process.env.R2_BUCKET_NAME,
+      Key: filename,
+      ContentType: fileType,
+    });
 
-
+    return await getSignedUrl(this.s3, command, { expiresIn: 3600 }); 
+  }
+}
 
     
-}
+
